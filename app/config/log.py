bd974bc3daa0cd2d88d6dac31e9280b83b11dd66
Alex Uglin <uglin76@mail.ru>
2022-01-30 14:16:13 +0300

    refactor log config

--- app/config/log.py
+++ app/config/log.py
@@ -4,51 +4,35 @@ from pathlib import Path
 
 from .app import settings
 
+LOG_LEVELS = ["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"]
 
-def set_log_level():
-    if settings.log_level not in ["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"]:
-        print("Check LOG_LEVEL values in .env file, the log level will be INFO")
-        settings.log_level = "INFO"
-    return settings.log_level
+# маппер для настройки вывода логов
+LOG_HANDLERS = {
+    "CONSOLE": lambda: (logging.StreamHandler(),),
+    "FILE": lambda: (logging.FileHandler(set_log_file_name()),),
+    "CONSOLE_AND_FILE": lambda: (logging.StreamHandler(), logging.FileHandler(set_log_file_name()),),
+}
 
 
 def set_log_file_name():
-    time_stamp = time.strftime("%Y-%m-%d-%H_%M_%S")
-    file_name = time_stamp + '.log'
+    file_name = time.strftime("%Y-%m-%d-%H_%M_%S") + '.log'
     dir_ = Path('./', 'logs')
-
     if not dir_.exists():
         dir_.mkdir()
-
     return Path(dir_, file_name)
 
 
-def set_log_handlers():
-    log_console = logging.StreamHandler()
-    log_file_name = set_log_file_name()
+# если LOG_OUTPUT не указан или указан неверно, то устанавливается вывод в CONSOLE
+handlers = LOG_HANDLERS[settings.log_output]() if settings.log_output in LOG_HANDLERS else LOG_HANDLERS["CONSOLE"]()
 
-    if settings.log_output == 'CONSOLE_AND_FILE':
-        log_file = logging.FileHandler(log_file_name)
-        handlers = (log_file, log_console)
-
-    elif settings.log_output == 'CONSOLE':
-        handlers = (log_console,)
-
-    elif settings.log_output == 'FILE':
-        log_file = logging.FileHandler(log_file_name)
-        handlers = (log_file,)
-
-    else:
-        print("Check LOG_OUTPUT values in .env file, the log will be output to the CONSOLE")
-        handlers = (log_console,)
-
-    return handlers
+# если LOG_LEVEL не указан или указан неверно, устанавливается уровень INFO
+level = settings.log_level if settings.log_level in LOG_LEVELS else "INFO"
 
 
 logging.basicConfig(
-    handlers=set_log_handlers(),
+    handlers=handlers,
     format=u'%(filename)+13s [ LINE:%(lineno)-4s] %(levelname)-8s [%(asctime)s] %(message)s',
-    level=set_log_level()
+    level=level
 )
 
 logger = logging.getLogger(__name__)
